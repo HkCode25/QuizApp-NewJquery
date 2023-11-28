f753097377bdbbefa833f262784fa8580255bd62
HardeepKaushal <hardeepk_fr@yahoo.fr>
2023-11-27 21:38:13 -0700

    com
    com

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,43 @@
-$('#quiz').quiz({
-  counterFormat: 'Question %current of %total',
-  questions: [
-    {
-      'q': 'Is jQuery required for this plugin?',
-      'options': [
-        'Yes',
-        'No'
-      ],
-      'correctIndex': 0,
-      'correctResponse': 'Good job, that was obvious.',
-      'incorrectResponse': 'Well, if you don\'t include it, your quiz won\'t work'
-    },
-    {
-      'q': 'How do you use it?',
-      'options': [
-        'Include jQuery, that\'s it!',
-        'Include jQuery and the plugin javascript.',
-        'Include jQuery, the plugin javascript, the optional plugin css, required markup, and the javascript configuration.'
-      ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Sounds more complicated than it really is.',
-      'incorrectResponse': 'Come on, it\'s not that easy!'
-    },
-    {
-      'q': 'The plugin can be configured to require a perfect score.',
-      'options': [
-        'True',
-        'False'
-      ],
-      'correctIndex': 0,
-      'correctResponse': 'You\'re a genius! You just set allowIncorrect to true.',
-      'incorrectResponse': 'Why you have no faith!? Just set allowIncorrect to true.'
-    },
-    {
-      'q': 'How do you specify the questions and answers?',
-      'options': [
-        'MySQL database',
-        'In the HTML',
-        'In the javascript configuration'
-      ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Refer to the documentation for the structure.',
-      'incorrectResponse': 'Wrong! Do it in the javascript configuration. You might need to read the documentation.'
-    }
-  ]
+function func1()
+{
+  $("#img1").fadeToggle(3000);
+}
+
+function func2()
+{
+  //$(element name).action()
+  //document.getElementByTagName(element name)
+
+  $("div").css('background-color', 'orange');
+  // $("#p1").css('font-style','italic');
+  //$(".para").css('font-style', 'italic')
+  //$(".para").css('font-family', 'arial')
+  //$(".para").css('font-size', '20px')
+  //$("#div1, #div3").css('background-color', 'green');
+  //$("#div1, li").css('background-color', 'purple')
+  //$("div p").fadeToggle();
+  //$("p:first").fadeToggle();
+  $("li:odd").fadeToggle();
+} 
+
+//Events in javascript
+function func3()
+{
+  document.getElementById("img2").style.width="500px"
+}
+
+//Events in jquery
+// $("button").click(func4);
+// function func4(){
+//     $("#img3").css('width','500px');
+// }
+
+
+$("document").ready(function(){
+
+  //anonymous function
+$("button").click(function(){
+  $("#img3").css('width','500px');
+});
+
 });
